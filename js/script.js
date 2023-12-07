8fb96fd3b23861cffabb65ba6f1a8b9d0c996163
Shah Tajbid Tousif <tajbidtousif@gmail.com>
2023-12-07 14:15:27 +0600

    Developing Home Section

--- js/script.js
+++ js/script.js
@@ -1,7 +1,34 @@
-let menubar = document.querySelector('#menubar');
-let navbar = document.querySelector('.navbar');
+let menubar = document.querySelector("#menubar");
+let navbar = document.querySelector(".navbar");
 
 menubar.onclick = () => {
-    menubar.classList.toggle('fa-xmark');
-    navbar.classList.toggle('active');
+  menubar.classList.toggle("fa-xmark");
+  navbar.classList.toggle("active");
+};
+
+window.onscroll = () => {
+  if (window.scrollY > 0) {
+    document.querySelector(".header").classList.add("active");
+  } else {
+    document.querySelector(".header").classList.remove("active");
+  }
+
+  menubar.classList.remove("fa-xmark");
+  menubar.classList.remove("active");
+};
+
+window.onload = () => {
+  if (window.scrollY > 0) {
+    document.querySelector(".header").classList.add("active");
+  } else {
+    document.querySelector(".header").classList.remove("active");
+  }
+
+  document.querySelector("#login-btn").onclick = () => {
+    document.querySelector(".login-form-container").classList.toggle("active");
+  };
+
+  document.querySelector("#close-login-btn").onclick = () => {
+    document.querySelector(".login-form-container").classList.remove("active");
+  };
 };
